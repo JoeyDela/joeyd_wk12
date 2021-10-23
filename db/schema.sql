78040f7c5fe5a4f39772a474d84fe81f6825a620
JoeyDela <duffy.west87@gmail.com>
2021-10-23 11:27:53 -0500

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,27 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
-SELECT DATABASE();
-
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name 
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id)
+);
+
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id)
 );
